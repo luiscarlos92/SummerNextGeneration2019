38937849af12707a22db4050430aa3721708cfc2
peper <peper@LAPTOP-J550ELHV>
2019-07-18 16:38:19 +0100

    Implementar primeiro ecra pontos vida

---
+++ SummerNextGeneration2019/Controllers/JogoController.cs
@@ -0,0 +1,27 @@
+﻿using SummerNextGeneration2019.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SummerNextGeneration2019.Controllers
+{
+    public class JogoController : Controller
+    {
+        // GET: Jogo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Tabuleiro()
+        {
+            ViewBag.PontosJogador1 = GameLogic.PontosJogador1;
+            ViewBag.PontosJogador2 = GameLogic.PontosJogador2;
+
+            return View();
+        }
+
+    }
+}
---
+++ SummerNextGeneration2019/Logic/GameLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SummerNextGeneration2019.Logic
+{
+    public static class GameLogic
+    {
+        public static int PontosJogador1 = 4000;
+        public static int PontosJogador2 = 4000;
+
+        public static int SubtrairPontos(int numeroJogador, int pontosDeAtaque)
+        {
+            if (numeroJogador == 1) 
+            {
+                PontosJogador1 -= pontosDeAtaque;
+                return PontosJogador1;
+            }
+            else
+            {
+                PontosJogador2 -= pontosDeAtaque;
+                return PontosJogador2;
+            }
+        }
+    }
+}
