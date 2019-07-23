113d39cb273c981312690ba4064a1bccc5a89c68
peper <peper@LAPTOP-J550ELHV>
2019-07-23 12:13:05 +0100

    update paginas installed package

--- SummerNextGeneration2019/Controllers/HomeController.cs
+++ SummerNextGeneration2019/Controllers/HomeController.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using YuGiOhDatabase;
 
 namespace SummerNextGeneration2019.Controllers
 {
     public class HomeController : Controller
     {
+
+        private static YuGiOhCardRepository _database;
+        public static YuGiOhCardRepository Repo
+        {
+            get
+            {
+                if (_database == null)
+                    _database = new YuGiOhCardRepository();
+                return _database;
+            }
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -15,6 +28,14 @@ namespace SummerNextGeneration2019.Controllers
 
         public ActionResult About()
         {
+            //var list = Repo.SearchForMonsters(new SearchFilter()
+            //{
+            //    Name = "dragon",
+            //    FuzzySearch = true
+            //}).ToList();
+
+            //var path = Repo.DownloadImage(list[0]);
+
             ViewBag.Message = "";
 
             return View();
@@ -23,6 +44,12 @@ namespace SummerNextGeneration2019.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Contacto dos jovens programadores.";
+
+            return View();
+        }
+        public ActionResult FoiAvisado()
+        {
+            ViewBag.Message = "";
 
             return View();
         }
