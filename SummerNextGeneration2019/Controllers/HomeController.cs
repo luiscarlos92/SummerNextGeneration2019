154a72d91dddb8353ac1e541367d7fa1f817e602
peper <peper@LAPTOP-J550ELHV>
2019-07-24 16:15:50 +0100

    ecra principal com monstros

--- SummerNextGeneration2019/Controllers/HomeController.cs
+++ SummerNextGeneration2019/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -20,9 +21,40 @@ namespace SummerNextGeneration2019.Controllers
                 return _database;
             }
         }
+        private string LoadImageToServer(string systemFilePath)
+
+        {
+
+            string downloadFilePath = systemFilePath;
+
+            string serverFolderPath = Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.GetData("DataDirectory").ToString()).FullName, "images");
+
+            if (!Directory.Exists(serverFolderPath))
+
+                Directory.CreateDirectory(serverFolderPath);
+
+            string fileName = Path.GetFileName(downloadFilePath);
+
+            string serverFilePath = Path.Combine(serverFolderPath, fileName);
+
+            if (!System.IO.File.Exists(serverFilePath))
+
+                System.IO.File.Copy(downloadFilePath, serverFilePath);
+
+            return fileName;
+
+        }
 
         public ActionResult Index()
         {
+            var list = Repo.SearchForMonsters(new SearchFilter()
+            {
+            }).ToList();
+
+            ViewBag.Image1 = LoadImageToServer(Repo.DownloadImage(list[new Random().Next(0, list.Count)]));
+            ViewBag.Image2 = LoadImageToServer(Repo.DownloadImage(list[new Random().Next(0, list.Count)]));
+            ViewBag.Image3 = LoadImageToServer(Repo.DownloadImage(list[new Random().Next(0, list.Count)]));
+
             return View();
         }
 
